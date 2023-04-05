d1ac76f7a302ea01ce9a57081ff886fceacfe505
Fernando <fh1273509@gmail.com>
2023-04-05 19:05:40 -0300

    configuramos la ruta raiz

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 
-
-
-
 const app = express();
+
+//configuramos el motor de plantillas
+
+
+//definimos las rutas
+app.get('/', (req, res)=>{
+    res.send('Hol fer')
+});
+
+
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
 });
