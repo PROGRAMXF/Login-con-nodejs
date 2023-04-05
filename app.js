822adae0fbfac43bf6d057e42fffaee58ebdd044
Fernando <fh1273509@gmail.com>
2023-04-05 19:14:07 -0300

    configuramos la carpeta public y node para procesar los datos

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 //configuramos el motor de plantillas
+app.set('view engine', 'ejs');
 
+//luego configuramos la carpeta public para archivos estaticos
+app.use(express.static('public'));
+
+//ahora configuramos node para que procese estos datos
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 //definimos las rutas
 app.get('/', (req, res)=>{
