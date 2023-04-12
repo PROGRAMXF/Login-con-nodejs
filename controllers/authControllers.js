e62c164841f4a76bf8395ce770abbcfbaceb39dc
Fernando <fh1273509@gmail.com>
2023-04-11 21:44:24 -0300

    registracion usuario con base de datos exitosa

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -5,10 +5,23 @@ const{promisify} = require('util'); //trabajamos con promesas / vamos a usar una
 
 //metodo para registrarnos
 exports.register = async (req, res)=>{
+try {
     const name = req.body.name;
     const user = req.body.user;
     const pass = req.body.pass;
     let passHash = await bcryptjs.hash(pass, 8)
    // console.log(passHash)
+   conexion.query('INSERT INTO users SET ?', {user:user, name:name, pass:passHash}, (error, results)=>{
+    if(error){
+        console.log(error)
+    }
+    res.redirect('/')
+   })
+}catch (error){
+    console.log(error);
+}
+
+
+
 
 }
